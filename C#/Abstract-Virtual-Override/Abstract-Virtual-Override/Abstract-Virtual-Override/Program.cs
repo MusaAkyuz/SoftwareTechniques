7c0f85d30080fea93ce3831fe1b0f16224b6ff6b
MusaAkyuz <musakyuz0@gmail.com>
2023-08-02 07:39:17 +0300

    adding abstract, virtual and override keywords usage

---
+++ C#/Abstract-Virtual-Override/Abstract-Virtual-Override/Abstract-Virtual-Override/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Abstract_Virtual_Override
+{
+    internal abstract class Animal
+    {
+        internal virtual void Sound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+    }
+
+    internal class Cat : Animal
+    { }
+
+    internal class Dog : Animal
+    {
+        internal override void Sound()
+        {
+            Console.WriteLine("Dog Sound");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Sound();
+
+            Dog dog = new Dog();
+            dog.Sound();
+        }
+    }
+}
