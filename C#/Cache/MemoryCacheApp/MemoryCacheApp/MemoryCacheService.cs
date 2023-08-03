936b03e446f50a2fc32c28ac2e3f1d0635739e38
MusaAkyuz <musakyuz0@gmail.com>
2023-08-03 07:24:33 +0300

    adding Memory Cache implementations

---
+++ C#/Cache/MemoryCacheApp/MemoryCacheApp/MemoryCacheService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace MemoryCacheApp
+{
+    internal class MemoryCacheService
+    {
+        private MemoryCache _cache;
+
+        public MemoryCacheService()
+        {
+            _cache = MemoryCache.Default;
+        }
+
+        public void AddItemToCache(string key, object value, int cacheDurationOnSecond)
+        {
+            var cacheDuration = TimeSpan.FromSeconds(cacheDurationOnSecond);
+            _cache.Add(key, value, DateTime.Now.Add(cacheDuration));
+        }
+
+        public object GetItemFromCache(string key)
+        {
+            return _cache.Get(key);
+        }
+
+        public void RemoveItemFromCache(string key)
+        {
+            if (_cache.Contains(key))
+            {
+                _cache.Remove(key);
+            }
+        }
+    }
+}
---
+++ C#/Cache/MemoryCacheApp/MemoryCacheApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MemoryCacheApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryCacheService memoryCacheService = new MemoryCacheService();
+
+            memoryCacheService.AddItemToCache("key1", "value1", 10);
+            Console.WriteLine(memoryCacheService.GetItemFromCache("key1"));
+            Thread.Sleep(5000);
+            Console.WriteLine(memoryCacheService.GetItemFromCache("key1"));
+            Thread.Sleep(6000);
+        }
+    }
+}
