207da620d7cfc5a026ac570bff44cc5b8bd76b78
Musa Akyüz <C9019183@arcelik.com>
2023-09-17 11:55:35 +0300

    Factory Pattern with Abstract classes

---
+++ C#/FactoryPattern/AbstractFactoryPattern/AbstractFactoryPattern/Creater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class Creater
+    {
+        public Game FactoryMethod(Games gameType)
+        {
+            Game game = null;
+            switch (gameType)
+            {
+                case Games.Atari:
+                    game = new Atari();
+                    break;
+                case Games.PC:
+                    game = new PC();
+                    break;
+                default: 
+                    throw new ArgumentException();
+            }
+            return game;
+        }
+    }
+
+    public enum Games
+    {
+        Atari,
+        PC
+    }
+}
---
+++ C#/FactoryPattern/AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Creater creater = new Creater();
+            Game atariOyunu = creater.FactoryMethod(Games.Atari);
+            Game pcOyunu = creater.FactoryMethod(Games.PC);
+
+            atariOyunu.Platform();
+            pcOyunu.Platform();
+
+            Console.Read();
+        }
+    }
+
+    class Atari : Game
+    {
+        public override void Platform()
+        {
+            Console.WriteLine("Atari GAME");
+        }
+    }
+
+    class PC : Game
+    {
+        public override void Platform()
+        {
+            Console.WriteLine("Pc GAME");
+        }
+    }
+}
