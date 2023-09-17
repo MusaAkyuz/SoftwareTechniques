6b505deb1406d73c9dc0e5f0dbcd6cb79720c700
Musa Akyüz <C9019183@arcelik.com>
2023-09-17 12:00:30 +0300

    Factory Pattern with interface

---
+++ C#/FactoryPattern/InterfaceFactoryPattern/InterfaceFactoryPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace InterfaceFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atari atari = new Atari();
+            atari.Platform();
+
+            PC pC = new PC();
+            pC.Platform();
+        }
+    }
+
+    interface Game
+    {
+        void Platform();
+    }
+
+    class Atari : Game
+    {
+        public void Platform()
+        {
+            Console.WriteLine("Atari game");
+        }
+    }
+
+    class PC : Game
+    {
+        public void Platform()
+        {
+            Console.WriteLine("PC game");
+        }
+    }
+}
